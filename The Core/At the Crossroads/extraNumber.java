cd670d0a83b60771d30d5799a0938045867a0e96
aashigangrade <aashigangrade06@gmail.com>
2023-08-27 20:39:06 +0530

    Added At the Crossroads in the CORE folder

---
+++ The Core/At the Crossroads/extraNumber.java
@@ -0,0 +1,42 @@
+You are given three integers, a, b and c. It is guaranteed that two of these integers are equal to each other. What is the value of the third integer?
+
+Example
+
+For a = 2, b = 7, and c = 2, the output should be
+solution(a, b, c) = 7.
+
+The two equal numbers are a and c. The third number (b) equals 7, which is the answer.
+
+Input/Output
+
+[execution time limit] 3 seconds (java)
+
+[memory limit] 1 GB
+
+[input] integer a
+
+Guaranteed constraints:
+1 ≤ a ≤ 109.
+
+[input] integer b
+
+Guaranteed constraints:
+1 ≤ b ≤ 109.
+
+[input] integer c
+
+Guaranteed constraints:
+1 ≤ c ≤ 109.
+
+[output] integer
+-----------------------
+
+int solution(int a, int b, int c) {
+    if (a == b) {
+        return c;
+    } else if (a == c) {
+        return b;
+    } else {
+        return a;
+    }
+}
